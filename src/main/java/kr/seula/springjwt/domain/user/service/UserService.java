b3c55daf3aad630d137d29006d9b05051b7b64e9
I'm not Seula <rheeseula@gmail.com>
2024-03-06 23:23:57 +0900

    Feat :: User 도메인 서비스 추가

---
+++ src/main/java/kr/seula/springjwt/domain/user/service/UserService.java
@@ -0,0 +1,15 @@
+package kr.seula.springjwt.domain.user.service;
+
+import jakarta.transaction.Transactional;
+import kr.seula.springjwt.domain.user.dto.RegisterDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class UserService {
+
+    public void register(RegisterDTO registerDTO) => {
+
+    }
+
+}
