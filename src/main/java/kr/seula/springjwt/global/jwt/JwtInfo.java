bde4d2664a81239add7f52716de86c62d6b2cfc8
I'm not Seula <rheeseula@gmail.com>
2024-03-07 08:19:07 +0900

    Feat :: JWT 인증 JwtInfo DTO 생성

---
+++ src/main/java/kr/seula/springjwt/global/jwt/JwtInfo.java
@@ -0,0 +1,14 @@
+package kr.seula.springjwt.global.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class JwtInfo {
+
+    private String accessToken;
+
+}
