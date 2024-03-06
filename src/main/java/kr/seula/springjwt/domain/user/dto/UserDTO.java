b78dc446af3a76391a174817f1f81cee777f8086
I'm not Seula <rheeseula@gmail.com>
2024-03-06 22:23:28 +0900

    Feat :: UserDTO 추가

---
+++ src/main/java/kr/seula/springjwt/domain/user/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package kr.seula.springjwt.domain.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+
+    private String username;
+
+    private String password;
+
+}
