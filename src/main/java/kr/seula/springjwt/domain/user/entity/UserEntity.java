8d2409678685f1cc4dfe4374f1bf44d21193b787
I'm not Seula <rheeseula@gmail.com>
2024-03-06 22:23:32 +0900

    Feat :: UserEntity 추가

---
+++ src/main/java/kr/seula/springjwt/domain/user/entity/UserEntity.java
@@ -0,0 +1,21 @@
+package kr.seula.springjwt.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity(name="userTable")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+}
