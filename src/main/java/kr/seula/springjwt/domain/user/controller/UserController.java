735cb2f40b91a89194dc18e04de2ee8baab004e9
I'm not Seula <rheeseula@gmail.com>
2024-03-06 23:19:16 +0900

    Feat :: User 도메인 Register 추가

--- src/main/java/kr/seula/springjwt/domain/user/controller/UserController.java
+++ src/main/java/kr/seula/springjwt/domain/user/controller/UserController.java
@@ -1,17 +1,25 @@
 package kr.seula.springjwt.domain.user.controller;
 
+import kr.seula.springjwt.domain.user.dto.LoginDTO;
+import kr.seula.springjwt.domain.user.dto.RegisterDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
-    @GetMapping("login")
-    public ResponseEntity<String> login() {
+    @PostMapping("login")
+    public ResponseEntity<String> login(LoginDTO loginDTO) {
         return ResponseEntity.ok("login successful");
+    }
+
+    @PostMapping("register")
+    public ResponseEntity<String> register(RegisterDTO registerDTO) {
+        return ResponseEntity.ok("register successful");
     }
 
 
