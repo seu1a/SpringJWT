2fc282d38064509b35ad15fb2a0dc09ab4d4983d
I'm not Seula <rheeseula@gmail.com>
2024-03-06 23:33:20 +0900

    Feat :: User 도메인 User 서비스 회원가입 추가

--- src/main/java/kr/seula/springjwt/domain/user/service/UserService.java
+++ src/main/java/kr/seula/springjwt/domain/user/service/UserService.java
@@ -2,14 +2,25 @@ package kr.seula.springjwt.domain.user.service;
 
 import jakarta.transaction.Transactional;
 import kr.seula.springjwt.domain.user.dto.RegisterDTO;
+import kr.seula.springjwt.domain.user.entity.UserEntity;
+import kr.seula.springjwt.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class UserService {
 
-    public void register(RegisterDTO registerDTO) => {
+    private final UserRepository userRepository;
 
+    public void register(RegisterDTO registerDTO) {
+        userRepository.save(
+                new UserEntity(
+                        registerDTO.getEmail(),
+                        registerDTO.getPassword()
+                )
+        );
     }
 
 }
