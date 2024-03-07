1831592cc7aff10354349b80deefdd936d320b40
I'm not Seula <rheeseula@gmail.com>
2024-03-07 10:41:16 +0900

    Feat :: Jwt UserDetailsImpl 생성

---
+++ src/main/java/kr/seula/springjwt/global/jwt/UserDetailsImpl.java
@@ -0,0 +1,57 @@
+package kr.seula.springjwt.global.jwt;
+
+import kr.seula.springjwt.domain.user.entity.UserEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final UserEntity userEntity;
+
+    public UserDetailsImpl(UserEntity userEntity) {
+        this.userEntity = userEntity;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+
+        authorities.add(userEntity::getRole);
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return userEntity.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return userEntity.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
