a13f2c1ee119540513daf6d74db12358d2fc26d7
I'm not Seula <rheeseula@gmail.com>
2024-03-07 12:14:53 +0900

    Feat :: Global BaseReponse DTO 추가

---
+++ src/main/java/kr/seula/springjwt/global/dto/BaseResponse.java
@@ -0,0 +1,16 @@
+package kr.seula.springjwt.global.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@AllArgsConstructor
+public class BaseResponse<T> {
+
+    private boolean success;
+    private String message;
+    private T data;
+
+}
