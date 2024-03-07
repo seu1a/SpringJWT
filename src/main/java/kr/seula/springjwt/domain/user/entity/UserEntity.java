26b81d905999d9c36e8c693634cd6b241490c05f
I'm not Seula <rheeseula@gmail.com>
2024-03-07 12:23:47 +0900

    Fix :: User 도메인 User 엔티티 테이블명 수정

--- src/main/java/kr/seula/springjwt/domain/user/entity/UserEntity.java
+++ src/main/java/kr/seula/springjwt/domain/user/entity/UserEntity.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@Entity(name="userTable")
+@Entity(name="user_table")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserEntity {
 
