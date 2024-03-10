c8ac261ac0e8fefcc4940757cec67daff193e992
seu1a <rheeseula@gmail.com>
2024-03-10 19:37:21 +0900

    Feat :: JwtProperties 생성

---
+++ src/main/java/kr/seula/springjwt/global/jwt/JwtProperties.java
@@ -0,0 +1,18 @@
+package kr.seula.springjwt.global.jwt;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@ConfigurationProperties("jwt")
+public class JwtProperties {
+
+    @Value("${jwt.secret}") private String secretKey;
+
+    @Value("${jwt.accessExpired}") private long accessExpired;
+
+    @Value("${jwt.refreshExpired}") private long refreshExpired;
+
+}
