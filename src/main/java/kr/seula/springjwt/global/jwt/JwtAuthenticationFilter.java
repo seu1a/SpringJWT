ebf38ec8346c1352f17d28509b7def6e2d8948d8
seu1a <rheeseula@gmail.com>
2024-03-10 19:44:26 +0900

    Refactor :: 이상한 나라의 임포트문 제거

--- src/main/java/kr/seula/springjwt/global/jwt/JwtAuthenticationFilter.java
+++ src/main/java/kr/seula/springjwt/global/jwt/JwtAuthenticationFilter.java
@@ -6,10 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
